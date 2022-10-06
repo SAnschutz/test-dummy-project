b4dc41a0663bae5a389b1b7c3c1c74aeab24e0cc
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2022-10-06 09:36:02 -0400

    just checking something

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Here are some MORE changes to the main branch</p>
+        <p>
+          Here are some MORE changes to the main branch and no changes
+          to branch I will be pulling this into
+        </p>
         <a
           className='App-link'
           href='https://reactjs.org'
